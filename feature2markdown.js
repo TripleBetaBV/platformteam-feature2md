da2a764de21af52ec6bd4ee8bb01a95c9194b647
J.Bijleveld <Jeroen@TripleBeta.nl>
2025-08-13 11:44:27 +0200

    Update feature2markdown.js

--- feature2markdown.js
+++ feature2markdown.js
@@ -104,7 +104,9 @@ function main() {
 // Export functions for testing
 export { findFeatureFiles, getBadgeTag, convertFeatureToMarkdown, main };
 
+main();
+
 // Run main function only if this file is executed directly
-if (import.meta.url === `file://${process.argv[1]}`) {
-  main();
-}
+//if (import.meta.url === `file://${process.argv[1]}`) {
+// main();
+//}
